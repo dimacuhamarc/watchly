da59a18899e0875ab1b23989c00dce19a90d47e5
Marc Dimacuha <dimacuha.marc@gmail.com>
2025-01-25 15:04:24 +0800

    Switch to Neon Serverless Postgres and update database configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
 
-export default function HomePage() {
+import { db } from "~/server/db";
+import { users } from "~/server/db/schema";
+
+export default async function HomePage() {
+  const userlist = await db.select().from(users);
+
+  console.log(userlist);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-emerald-500 to-emerald-800 text-white">
       <h1>Watchly</h1>
+      <pre>{JSON.stringify(userlist, null, 2)}</pre>
     </main>
   );
 }
