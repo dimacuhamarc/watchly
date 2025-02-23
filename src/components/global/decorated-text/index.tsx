d93bffc20dd1e36626bd32c806e436a5b257f9d7
Marc <dimacuha.marc@gmail.com>
2025-02-24 04:12:16 +0800

    feat: Add DecoratedTextWithIcon component for flexible text display

---
+++ src/components/global/decorated-text/index.tsx
@@ -0,0 +1,3 @@
+import { default as DecoratedTextWithIcon } from "./textWithIcon";
+
+export { DecoratedTextWithIcon };
---
+++ src/components/global/decorated-text/textWithIcon/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface TextWithIconProps {
+  text: string;
+  icon: React.ReactNode;
+}
+
+function TextWithIcon({ text, icon }: TextWithIconProps) {
+  return (
+    <p className="flex flex-row items-center gap-2 text-sm md:text-base">
+      <span className="mb-0.5">{icon}</span>
+      <span>{text}</span>
+    </p>
+  );
+}
+
+export default TextWithIcon;
