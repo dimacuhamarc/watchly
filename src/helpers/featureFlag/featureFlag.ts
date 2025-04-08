d39bde76c9526e019e8e4ce98f2e6bfa383236e3
Marc <dimacuha.marc@gmail.com>
2025-04-09 04:01:41 +0800

    feat: add feature flag helper functions

---
+++ src/helpers/featureFlag/featureFlag.ts
@@ -0,0 +1,6 @@
+import { FEATURE_FLAGS } from "./features";
+import type { FeatureFlags } from "./features";
+
+export const isFeatureEnabled = (featureName: keyof FeatureFlags): boolean => {
+  return !!FEATURE_FLAGS[featureName];
+}
