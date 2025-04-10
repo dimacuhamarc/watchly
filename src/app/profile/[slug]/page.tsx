04c85f59d72196a1d5a1f03e69fc7382eee974fa
Marc Dimacuha <dimacuha.marc@gmail.com>
2025-04-10 23:04:08 +0800

    feat: refactor profile components; replace ProfileV2 with Profile and remove unused files

--- src/app/profile/[slug]/page.tsx
+++ src/app/profile/[slug]/page.tsx
@@ -1,14 +1,14 @@
 
 import React from 'react'
 import AuthLayoutProvider from '~/components/layout/authLayoutProvider';
-import ProfileV2 from '~/components/resource/profile/profilev2';
+import Profile from '~/components/resource/profile/index';
 
 function ProfilePage({params}: { params: { slug: string } }) {
   const { slug }: { slug: string } = React.use(Promise.resolve(params));
   return (
     <AuthLayoutProvider>
       <div className="mx-auto flex min-h-screen max-w-screen-xl flex-col items-start justify-start py-28 md:px-0">
-        <ProfileV2 params={slug} />
+        <Profile params={slug} />
       </div>
     </AuthLayoutProvider>
   );
--- src/components/resource/profile/index.tsx
+++ src/components/resource/profile/index.tsx
@@ -1,66 +1,62 @@
 "use client";
 
-import React from "react";
-import { useEffect, useState } from "react";
-import { InitialAvatar, PhotoAvatar } from "~/components/global/avatars";
-import { useAuthenticated } from "~/hooks/useAuth";
-import { fetchUserData } from "~/utils/api/apiRequests";
-import { formatToYearMonth } from "~/helpers/date";
-import type { SanitizedUserData } from "~/utils/types/data";
-import { FaEllipsisH, FaShareAlt } from "react-icons/fa";
-import ActivityList from "~/components/resource/profile/Activity";
-import UserAbout from "~/components/resource/profile/About";
-import UserMilestones from "~/components/resource/profile/Milestones";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import React from "react";
+import { useAuthenticated } from "~/hooks/useAuth";
+import { useProfile } from "~/hooks/useProfile";
 
-function ProfileComponent() {
-  const { userData: userDataFromCookie, userFollowData } = useAuthenticated();
-  const [userData, setUserData] = useState<SanitizedUserData | null>(null);
+import { formatToYearMonth } from "~/helpers/date";
+import { InitialAvatar, PhotoAvatar } from "~/components/global/avatars";
+import { FaEllipsisH, FaLink, FaShareAlt } from "react-icons/fa";
+
+import ActivityList from "./Activity";
+import UserAbout from "./About";
+import UserMilestones from "./Milestones";
+import copyToClipboard from "~/helpers/clipboard";
+
+interface ProfileProps {
+  params: string;
+}
+
+function Profile({ params }: ProfileProps) {
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
-
-  const userDataName = userData ? `${userData.first_name}` : "User";
-
+  const { username, cookiesLoaded } = useAuthenticated();
+  const currentUsername = username ?? "";
+  const { isCurrentUser, profileLoaded, profileData } = useProfile(
+    params,
+    currentUsername,
+  );
+  const userDataName = profileData ? `${profileData.first_name}` : "User";
   useEffect(() => {
-    const fetchData = async () => {
-      if (
-        userDataFromCookie &&
-        typeof userDataFromCookie === "object" &&
-        "id" in userDataFromCookie
-      ) {
-        try {
-          const data = await fetchUserData(userDataFromCookie.id);
-          if (!data) {
-            throw new Error("No user data found");
-          }
-          setUserData(data);
-        } catch (err) {
-          setError(true);
-          setErrorMessage("Failed to fetch user data");
-          console.error("Error fetching user data:", err);
-        } finally {
-          setLoading(false);
-        }
-      }
-    };
+    if (profileData === null) {
+      setError(true);
+    }
+    if (!cookiesLoaded && !profileLoaded) {
+      setLoading(true);
+    }
+  }, [profileData, cookiesLoaded, profileLoaded]);
 
-    void fetchData();
-  }, [userDataFromCookie]);
-
-  if (loading) {
+  if (!profileLoaded) {
     return (
-      <>
-        <div className="mx-auto my-auto flex flex-col items-center justify-center gap-4">
-          <span className="loading loading-infinity loading-lg"></span>
-          {!error && <h1>Loading Profile</h1>}
-          {error && (
-            <div className="text-error">
-              <span>Error Loading Profile! {errorMessage}</span>
-            </div>
-          )}
-        </div>
-      </>
+      <div className="mx-auto my-auto flex flex-col items-center justify-center gap-4">
+        <span className="loading loading-infinity loading-lg"></span>
+        <h1>Loading Profile</h1>
+      </div>
+    );
+  }
+
+  if (profileData === null) {
+    return (
+      <div className="mx-auto my-auto flex flex-col items-center justify-center gap-4 text-error">
+        <span className="font-bold">Error Loading Profile</span>
+        <p>The requested profile could not be found.</p>
+        <p>Please check the URL or try again later.</p>
+        <Link href="/" className="btn btn-outline btn-error">
+          Go to Home
+        </Link>
+      </div>
     );
   }
 
@@ -69,45 +65,59 @@ function ProfileComponent() {
       <Link href="/" className="mb-4 text-sm text-gray-500 hover:text-gray-200">
         Back to Home
       </Link>
-      <div className="flex w-full flex-row items-center gap-4 rounded-t-md bg-white px-20 pb-6 pt-12 text-slate-900 shadow-md lg:pt-16">
-        {userData?.profile_picture && (
-          <div className="relative h-24 w-24 overflow-hidden rounded-full">
+      <div className="flex w-full flex-row items-center gap-8 rounded-t-md bg-white px-20 pb-6 pt-12 text-slate-900 shadow-md lg:pt-16">
+        {profileData?.profile_picture && (
+          <div className="relative h-32 w-32 overflow-hidden rounded-full">
             <PhotoAvatar
-              src={userData?.profile_picture}
-              alt={userDataName}
+              src={profileData?.profile_picture}
+              alt={profileData?.username}
               isAutoSized={true}
               className="h-full w-full object-cover"
             />
           </div>
         )}
-        {!userData?.profile_picture && (
+        {!profileData?.profile_picture && (
           <InitialAvatar
-            name={userData?.first_name + " " + userData?.last_name}
+            name={profileData?.first_name + " " + profileData?.last_name}
           />
         )}
         <div className="flex flex-col">
           <h1 className="text-3xl font-semibold leading-none">
-            {userData?.first_name + " " + userData?.last_name}
+            {profileData?.first_name + " " + profileData?.last_name}
           </h1>
           <h2 className="text-md">
-            <span className="text-primary">@{userData?.username}</span> |{" "}
-            <span className="text-slate-900">
-              Joined{" "}
-              {formatToYearMonth(userData?.created_at?.toLocaleString() ?? "")}
-            </span>
+            <span className="text-primary">@{profileData?.username}</span>
           </h2>
           <h2 className="text-md flex flex-row gap-2">
-            <span className="text-primary">
-              <span className="font-bold">{userFollowData?.followers}</span> Followers
-            </span>
+            <Link href={"/"} className="link text-primary">
+              <span className="font-bold">{profileData?.followers}</span>{" "}
+              Followers
+            </Link>
+            <Link href={"/"} className="link text-slate-900">
+              <span className="font-bold">{profileData?.followers}</span>{" "}
+              Followers
+            </Link>
+          </h2>
+          <h2 className="text-md">
             <span className="text-slate-900">
-              <span className="font-bold">{userFollowData?.following}</span> Following
+              Joined{" "}
+              {formatToYearMonth(
+                profileData?.created_at?.toLocaleString() ?? "",
+              )}
             </span>
           </h2>
         </div>
         <div className="mb-auto ml-auto flex flex-col gap-2 lg:flex-row">
-          <button className="flex h-8 w-8 items-center justify-center rounded-full text-slate-900 hover:bg-slate-200">
-            <FaShareAlt />
+          <button
+            className="flex h-8 w-8 items-center justify-center rounded-full text-slate-900 hover:bg-slate-200"
+            onClick={(event) => {
+              event.preventDefault();
+              void copyToClipboard(
+                `${window.location.origin}/profile/${profileData?.username}`,
+              );
+            }}
+          >
+            <FaLink />
           </button>
           <button className="flex h-8 w-8 items-center justify-center rounded-full text-slate-900 hover:bg-slate-200">
             <FaEllipsisH />
@@ -115,10 +125,10 @@ function ProfileComponent() {
         </div>
       </div>
       <div className="flex w-full flex-row gap-4 bg-white px-20 pb-8 text-slate-900 shadow-md">
-        {false ? (
-          <button className="btn btn-primary">Follow</button>
-        ) : (
+        {isCurrentUser ? (
           <button className="btn btn-primary">Edit Profile</button>
+        ) : (
+          <button className="btn btn-primary">Follow</button>
         )}
         <button className="btn">
           Watchlists
@@ -131,7 +141,7 @@ function ProfileComponent() {
       </div>
       <div className="flex w-full flex-col gap-14 rounded-b-md bg-white px-4 pb-8 text-slate-900 shadow-md md:flex-row md:px-8 lg:px-16">
         <div className="flex w-full flex-col gap-4">
-          <UserAbout userData={userData} />
+          <UserAbout userData={profileData} />
           <ActivityList userDataName={userDataName} />
         </div>
         <UserMilestones />
@@ -140,4 +150,4 @@ function ProfileComponent() {
   );
 }
 
-export default ProfileComponent;
+export default Profile;
