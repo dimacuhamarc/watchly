b18be0352bb461a87a1c048357921880e9df15d2
Marc Dimacuha <dimacuha.marc@gmail.com>
2025-04-20 12:02:37 +0800

    feat: add InformationScreen component and enhance screen props for better user feedback

--- src/components/utility/screens/index.tsx
+++ src/components/utility/screens/index.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
-import { FaArrowLeft } from "react-icons/fa";
-import { LuTriangleAlert } from "react-icons/lu";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { LuIdCard, LuTriangleAlert } from "react-icons/lu";
 
 interface CommonScreenProps {
   loadingMessage?: string;
+  message?: string;
+  details?: string;
   errorMessage?: string;
   errorDetails?: string;
   redirectLink?: {
     href: string;
     label: string;
   }
+  leftIcon?: boolean;
+  rightIcon?: boolean;
 }
 
 
@@ -22,17 +26,34 @@ const LoadingScreen = ({ loadingMessage }: CommonScreenProps) => {
   );
 }
 
-const UnauthorizedAccess = ({ errorMessage, errorDetails, redirectLink }: CommonScreenProps) => {
+const UnauthorizedAccess = ({ errorMessage, errorDetails, redirectLink, leftIcon, rightIcon }: CommonScreenProps) => {
   return (
     <div className="mx-auto my-auto flex flex-col items-center justify-center gap-4 text-error md:h-full md:w-full h-screen w-screen">
       <LuTriangleAlert className="text-6xl" />
       <span className="font-bold">{errorMessage}</span>
       <p>{errorDetails}</p>
       <Link href={redirectLink?.href ?? `/`} className="btn btn-outline btn-error">
-        <FaArrowLeft />{redirectLink?.label}
+        {leftIcon && <FaArrowLeft />}
+        {redirectLink?.label}
+        {rightIcon && <FaArrowRight />}
       </Link>
     </div>
   );
 }
 
-export { LoadingScreen, UnauthorizedAccess };
+const InformationScreen = ({ message, details, redirectLink, leftIcon, rightIcon }: CommonScreenProps) => {
+  return (
+    <div className="mx-auto my-auto flex flex-col items-center justify-center gap-4 md:h-full md:w-full h-screen w-screen text-slate-100">
+      <LuIdCard className="text-6xl" />
+      <span className="font-bold">{message}</span>
+      <p>{details}</p>
+      <Link href={redirectLink?.href ?? `/`} className="btn hover:btn-outline btn-slate-100">
+        {leftIcon && <FaArrowLeft />}
+        {redirectLink?.label}
+        {rightIcon && <FaArrowRight />}
+      </Link>
+    </div>
+  );
+}
+
+export { LoadingScreen, UnauthorizedAccess, InformationScreen };
